e322858f069f161fcffdc1612a16a16259a584ba
roywu <kmc9330071>
2023-03-27 15:12:55 +0800

    [feat] add login and logout

--- pigeon/home/urls.py
+++ pigeon/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, score_quanmin, score_3678
 
 app_name = 'home'
 urlpatterns = [
-    path('', index, name='home'),
+    path('', index, name='index'),
+    path('score/quanmin/', score_quanmin, name='score_quanmin'),
+    path('score/3678/', score_3678, name='score_3678'),
 ]
--- pigeon/home/views.py
+++ pigeon/home/views.py
@@ -1,9 +1,29 @@
 
 from django.shortcuts import render
 
+from cb.auth_mgr.decorators import func_required
+
 
 def index(request):
-    context = {'logs': []}
+    context = {}
 
     html = 'home/index.html'
+    return render(request, html, context)
+
+
+@func_required('func_score_quanmin')
+def score_quanmin(request):
+    name = '全民海翔聯合會'
+    context = {"name": name}
+
+    html = 'home/score.html'
+    return render(request, html, context)
+
+
+@func_required('func_score_3678')
+def score_3678(request):
+    name = '3678聯合會'
+    context = {"name": name}
+
+    html = 'home/score.html'
     return render(request, html, context)
